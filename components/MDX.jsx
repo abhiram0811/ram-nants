c8205a4d04aec3309b7f1073c47f780b55976561
Abhiram Mulinti <abhirammulinti@Abhirams-MacBook-Air.local>
2026-01-02 16:57:06 -0700

    Fix markdown rendering using markdown-to-jsx

--- components/MDX.jsx
+++ components/MDX.jsx
@@ -1,4 +1,5 @@
 import TopNav from "./TopNav";
+import Markdown from "markdown-to-jsx";
 
 export default function MDX(props) {
     const { text } = props // gives us access to the text attribute (or the value assigned to it really)
@@ -6,9 +7,11 @@ export default function MDX(props) {
         <section className="mdx-container">
             <TopNav {...props} />
             <article>
-
-                    {text.trim() || 'Hop in the editor to create a new note'}
-
+                {text.trim() ? (
+                    <Markdown>{text}</Markdown>
+                ) : (
+                    'Hop in the editor to create a new note'
+                )}
             </article>
         </section>
     )
